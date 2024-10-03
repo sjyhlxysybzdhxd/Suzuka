9630766511d1951e674b4998000d51b2c1ced57d
WhitePaper233 <1423634265@qq.com>
2024-10-03 16:34:20 +0800

    登录页非逻辑功能实现

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,40 @@
-import './assets/main.css'
 import './styles/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { PrimeVue } from '@primevue/core'
+import { definePreset } from '@primevue/themes'
+import Aura from '@primevue/themes/aura'
 
 import App from './App.vue'
 import router from './router'
+
+const AuraSkyTheme = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{sky.50}',
+      100: '{sky.100}',
+      200: '{sky.200}',
+      300: '{sky.300}',
+      400: '{sky.400}',
+      500: '{sky.500}',
+      600: '{sky.600}',
+      700: '{sky.700}',
+      800: '{sky.800}',
+      900: '{sky.900}',
+      950: '{sky.950}'
+    }
+  }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: AuraSkyTheme
+  }
+})
 
 app.mount('#app')
